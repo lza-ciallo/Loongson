fa81e060d516469a5be68cc1da5d8198ee4550e1
ZiAng LIN <linziang100@foxmail.com>
2025-07-03 16:24:41 +0800

    Delete 多发射原型机v2 directory

--- 多发射原型机v2/INST_MEM.sv
+++
@@ -1,9 +0,0 @@
-module INST_MEM (
-    input               rst,
-    input   [7 : 0]     pc,
-    output  [9 : 0]     inst_x,
-    output  [9 : 0]     inst_y,
-    output  [9 : 0]     inst_z
-);
-
-endmodule
--- 多发射原型机v2/PC.sv
+++
@@ -1,9 +0,0 @@
-module PC (
-    input               clk,
-    input               rst,
-    input               freeze_front,
-    output  [7 : 0]     pc,
-    output              valid_pc
-);
-
-endmodule
